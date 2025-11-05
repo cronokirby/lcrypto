6166bf3d786a984987ae2147678fca78ad5afe04
Lucas Meier <lucas@cronokirby.com>
2025-11-04 21:43:32 -0800

    Define monad instance

--- Lcrypto/Basic.lean
+++ Lcrypto/Basic.lean
@@ -1,10 +1,42 @@
-inductive Program (α : Type) where
-| Pure : α → Program α
-| Sample : (Bool → Program α) → Program α
+inductive Program (R : Type) (α : Type) where
+| Pure : α → Program R α
+| Sample : (R → Program R α) → Program R α
+| Eq : R → R → (Bool → Program R α) → Program R α
 
-def Program.map (f : α → β) (prog : Program α) : Program β := match prog with
-  | Program.Pure a => Program.Pure (f a)
-  | Program.Sample k => Program.Sample (fun s => map f (k s))
+def eval : (∀ R, Program R α) → α :=
+  let rec inner (s : Nat) (p : Program Nat α) : α := match p with
+    | Program.Pure a => a
+    | Program.Sample φ => inner (s.succ) (φ s)
+    | Program.Eq r1 r2 φ => inner s (φ (r1 == r2))
+  fun p => inner 0 (p Nat)
 
-instance : Functor Program where
-  map := Program.map
+section
+
+variable {R : Type}
+
+def Program.sample : Program R R := Program.Sample Program.Pure
+
+def Program.eq (x : R) (y : R) : Program R Bool := Program.Eq x y Program.Pure
+
+instance : Functor (Program R) where
+  map :=
+    let rec inner {α β} (f : α → β) (p : Program R α) : Program R β := match p with
+      | Program.Pure a => Program.Pure (f a)
+      | Program.Sample φ => Program.Sample (fun r => inner f (φ r))
+      | Program.Eq r1 r2 φ => Program.Eq r1 r2 (fun b => inner f (φ b))
+    inner
+
+instance : Monad (Program R) where
+  pure := Program.Pure
+  bind :=
+    let rec inner {α β} (p : Program R α) (f : α → Program R β) : Program R β := match p with
+      | Program.Pure a => f a
+      | Program.Sample φ => Program.Sample (fun r => inner (φ r) f)
+      | Program.Eq r1 r2 φ => Program.Eq r1 r2 (fun b => inner (φ b) f)
+    inner
+
+def foo : Program R Bool := do
+  let r <- Program.sample
+  Program.eq r r
+
+end section
