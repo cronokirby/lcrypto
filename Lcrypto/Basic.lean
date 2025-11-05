e3e2ade8967f46692f815764ef79b38fd54e73f5
Lucas Meier <lucas@cronokirby.com>
2025-11-04 20:58:09 -0800

    Define basic Program Monad

--- Lcrypto/Basic.lean
+++ Lcrypto/Basic.lean
@@ -1,1 +1,10 @@
-def hello := "world"
+inductive Program (α : Type) where
+| Pure : α → Program α
+| Sample : (Bool → Program α) → Program α
+
+def Program.map (f : α → β) (prog : Program α) : Program β := match prog with
+  | Program.Pure a => Program.Pure (f a)
+  | Program.Sample k => Program.Sample (fun s => map f (k s))
+
+instance : Functor Program where
+  map := Program.map
