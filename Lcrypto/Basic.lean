3a6be04c2bc6fecf0cf6a36e25fb69e2f6184246
Lucas Meier <lucas@cronokirby.com>
2025-11-05 21:14:47 -0800

    Add some basic proofs around monad instance

--- Lcrypto/Basic.lean
+++ Lcrypto/Basic.lean
@@ -10,13 +10,13 @@ def eval : (∀ R, Program R α) → α :=
     | Program.Eq r1 r2 φ => inner s (φ (r1 == r2))
   fun p => inner 0 (p Nat)
 
-section
+namespace Program
 
 variable {R : Type}
 
-def Program.sample : Program R R := Program.Sample Program.Pure
+def sample : Program R R := Program.Sample Program.Pure
 
-def Program.eq (x : R) (y : R) : Program R Bool := Program.Eq x y Program.Pure
+def eq (x : R) (y : R) : Program R Bool := Program.Eq x y Program.Pure
 
 instance : Functor (Program R) where
   map :=
@@ -26,7 +26,7 @@ instance : Functor (Program R) where
       | Program.Eq r1 r2 φ => Program.Eq r1 r2 (fun b => inner f (φ b))
     inner
 
-instance : Monad (Program R) where
+instance monad : Monad (Program R) where
   pure := Program.Pure
   bind :=
     let rec inner {α β} (p : Program R α) (f : α → Program R β) : Program R β := match p with
@@ -36,7 +36,23 @@ instance : Monad (Program R) where
     inner
 
 def foo : Program R Bool := do
-  let r <- Program.sample
-  Program.eq r r
+  let r <- sample
+  eq r r
 
-end section
+def foo_test : eval @foo = true := by
+  rw [eval]
+  rw [eval.inner.eq_def]
+  rw [foo]
+  rw [bind]
+  rw [sample]
+  rw [monad]
+  simp
+  rw [monad.inner]
+  simp
+  rw [monad.inner]
+  rw [eq]
+  rw [eval.inner.eq_def]
+  simp
+  rw [eval.inner.eq_def]
+
+end Program
